4f9eff91f25c65c1e8c25a70fee768bd5bb0f85d
Péricles Pires <periclespires@protonmail.com>
2023-06-11 20:22:20 -0300

    navigation on logo fixed

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -15,7 +15,7 @@ export default function navbar(props) {
     </nav>
     <header class="py-3 mb-4 border-bottom">
       <div class="container d-flex flex-wrap justify-content-center">
-        <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+        <a onclick="window.utils.navigate('home', { page: 1})" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none" style="cursor: pointer">
           <span class="fs-4"><img src="../../img/logo.png" width="60px" /></span>
         </a>
         <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
